7dc0226373e984c194444752ded0ed62aeb36807
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-11-06 09:14:35 +0100

    Fix sitemap monkey patch being broken for <priority>

--- trading_strategy_docs/sphinx_sitemap_patch.py
+++ trading_strategy_docs/sphinx_sitemap_patch.py
@@ -2,10 +2,16 @@
 
 """
 
-import logging
+import os
 import queue
+from multiprocessing import Manager
+from pathlib import Path
+from typing import Any, Dict, List, Optional
+from xml.etree import ElementTree
+import logging
 
-import xml.etree.ElementTree as ET
+from sphinx.application import Sphinx
+from sphinx.util.logging import getLogger
 
 import sphinx_sitemap
 from sphinx_sitemap import get_locales, hreflang_formatter
@@ -15,8 +21,87 @@ PRIORITY = 0.8
 logger = logging.getLogger(__name__)
 
 
-def _create_sitemap_patched(app, exception):
-    """Generates the sitemap.xml from the collected HTML page links"""
+# def _create_sitemap_patched_old(app, exception):
+#     """Generates the sitemap.xml from the collected HTML page links"""
+#     site_url = app.builder.config.site_url or app.builder.config.html_baseurl
+#     if site_url:
+#         site_url.rstrip("/") + "/"
+#     else:
+#         logger.warning(
+#             "sphinx-sitemap: html_baseurl is required in conf.py." "Sitemap not built.",
+#             type="sitemap",
+#             subtype="configuration",
+#         )
+#         return
+#
+#     env = app.builder.env
+#     import ipdb ; ipdb.set_trace()
+#     if env.sitemap_links.empty():
+#         logger.info(
+#             "sphinx-sitemap: No pages generated for %s" % app.config.sitemap_filename,
+#             type="sitemap",
+#             subtype="information",
+#         )
+#         return
+#
+#     ET.register_namespace("xhtml", "http://www.w3.org/1999/xhtml")
+#
+#     root = ET.Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
+#
+#     locales = get_locales(app, exception)
+#
+#     if app.builder.config.version:
+#         version = app.builder.config.version + "/"
+#     else:
+#         version = ""
+#
+#     while True:
+#         try:
+#             link = env.sitemap_links.get_nowait()
+#         except queue.Empty:
+#             break
+#
+#         url = ET.SubElement(root, "url")
+#         scheme = app.config.sitemap_url_scheme
+#         if app.builder.config.language:
+#             lang = app.builder.config.language + "/"
+#         else:
+#             lang = ""
+#
+#         ET.SubElement(url, "loc").text = site_url + scheme.format(
+#             lang=lang, version=version, link=link
+#         )
+#
+#         ET.SubElement(url, "priority").text = str(PRIORITY)
+#
+#         for lang in locales:
+#             lang = lang + "/"
+#             ET.SubElement(
+#                 url,
+#                 "{http://www.w3.org/1999/xhtml}link",
+#                 rel="alternate",
+#                 hreflang=hreflang_formatter(lang.rstrip("/")),
+#                 href=site_url + scheme.format(lang=lang, version=version, link=link),
+#             )
+#
+#     filename = app.outdir + "/" + app.config.sitemap_filename
+#     ET.ElementTree(root).write(
+#         filename, xml_declaration=True, encoding="utf-8", method="xml"
+#     )
+#
+#     logger.info(
+#         "sphinx-sitemap: %s was generated for URL %s in %s"
+#         % (app.config.sitemap_filename, site_url, filename),
+#         type="sitemap",
+#         subtype="information",
+#     )
+
+def _create_sitemap_patched(app: Sphinx, exception):
+    """
+    Generates the sitemap.xml from the collected HTML page links.
+
+    :param app: The Sphinx Application instance
+    """
     site_url = app.builder.config.site_url or app.builder.config.html_baseurl
     if site_url:
         site_url.rstrip("/") + "/"
@@ -28,8 +113,7 @@ def _create_sitemap_patched(app, exception):
         )
         return
 
-    env = app.builder.env
-    if env.sitemap_links.empty():
+    if app.env.app.sitemap_links.empty():  # type: ignore
         logger.info(
             "sphinx-sitemap: No pages generated for %s" % app.config.sitemap_filename,
             type="sitemap",
@@ -37,11 +121,13 @@ def _create_sitemap_patched(app, exception):
         )
         return
 
-    ET.register_namespace("xhtml", "http://www.w3.org/1999/xhtml")
+    ElementTree.register_namespace("xhtml", "http://www.w3.org/1999/xhtml")
 
-    root = ET.Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
+    root = ElementTree.Element(
+        "urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    )
 
-    locales = get_locales(app, exception)
+    locales = get_locales(app)
 
     if app.builder.config.version:
         version = app.builder.config.version + "/"
@@ -50,26 +136,26 @@ def _create_sitemap_patched(app, exception):
 
     while True:
         try:
-            link = env.sitemap_links.get_nowait()
+            link = app.env.app.sitemap_links.get_nowait()  # type: ignore
         except queue.Empty:
             break
 
-        url = ET.SubElement(root, "url")
+        url = ElementTree.SubElement(root, "url")
         scheme = app.config.sitemap_url_scheme
         if app.builder.config.language:
             lang = app.builder.config.language + "/"
         else:
             lang = ""
 
-        ET.SubElement(url, "loc").text = site_url + scheme.format(
-            lang=lang, version=version, link=link
+        ElementTree.SubElement(url, "loc").text = site_url + scheme.format(
+            lang=lang, version=version, link=link,
         )
 
-        ET.SubElement(url, "priority").text = str(PRIORITY)
+        ElementTree.SubElement(url, "priority").text = str(PRIORITY)
 
         for lang in locales:
             lang = lang + "/"
-            ET.SubElement(
+            ElementTree.SubElement(
                 url,
                 "{http://www.w3.org/1999/xhtml}link",
                 rel="alternate",
@@ -77,8 +163,8 @@ def _create_sitemap_patched(app, exception):
                 href=site_url + scheme.format(lang=lang, version=version, link=link),
             )
 
-    filename = app.outdir + "/" + app.config.sitemap_filename
-    ET.ElementTree(root).write(
+    filename = Path(app.outdir) / app.config.sitemap_filename
+    ElementTree.ElementTree(root).write(
         filename, xml_declaration=True, encoding="utf-8", method="xml"
     )
 
