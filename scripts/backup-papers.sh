0a494c72a949603c11cb8658f71a2cf5885f4eff
Mikko Ohtamaa <mikko@redinnovation.com>
2026-06-12 11:56:17 +0200

    Make backup script two ways

--- scripts/backup-papers.sh
+++ scripts/backup-papers.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #
-# Copy papers to the server
+# Copy papers to the server and fetch new PDFs from the server
 
-rsync -av --inplace articles mikko@vitalik7-tailscale:./code/docs/
+set -euo pipefail
+
+REMOTE="mikko@vitalik7-tailscale:./code/docs"
+
+rsync -av --inplace articles "$REMOTE/"
+
+rsync -av --inplace --ignore-existing \
+  --include='*/' \
+  --include='*.pdf' \
+  --exclude='*' \
+  "$REMOTE/articles/" articles/
